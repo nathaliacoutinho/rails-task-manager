2a844f6d543fab244dc7dee6bed38f9cc1eed242
nathalia coutinho <nathccoutinhos@gmail.com>
2018-06-05 13:01:42 +0200

    add update and delete options to the task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
 end
+
+  # read: show all, show one
+  # create: new, create
+  # update: edit, update
+  # destroy: destroy
+  # get '/tasks/new', to: "tasks#new"
+  # get '/tasks/:id', to: "tasks#show"
+  # post '/tasks', to: "tasks#create"
+  # get '/tasks/:id/edit', to: "tasks#edit"
+  # patch '/tasks/:id', to: "tasks#update"
+  # delete '/tasks/:id', to: "tasks#destroy"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Task.create!(
+#   [
+#     {
+#     title: "Laundry",
+#     details: "Do not mix the colors",
+#     },
+#     {
+#       title: "Water the plants",
+#       details: "Remember the one in the halway",
+#     },
+#     {
+#       title: "Groceries",
+#       details: "Toothpaste",
+#     }
+#   ]
+# )
